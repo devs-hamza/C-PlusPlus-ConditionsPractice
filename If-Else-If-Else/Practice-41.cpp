fa421c960fe9686bfad35903ef36f11c761db0f3
Hamza <alihamza031340@gmail.com>
2026-09-24 21:25:43 +0500

    Organize Practice-41.cpp

---
+++ If-Else-If-Else/Practice-41.cpp
@@ -0,0 +1,4 @@
+// Calculate Salary Bonus
+#include <iostream>
+using namespace std;
+int main(){float salary,bonus;cout<<"Enter salary: ";cin>>salary;if(salary<0)cout<<"Invalid salary";else if(salary>=100000){bonus=salary*0.10;cout<<"Bonus = "<<bonus;}else if(salary>=50000){bonus=salary*0.07;cout<<"Bonus = "<<bonus;}else{bonus=salary*0.05;cout<<"Bonus = "<<bonus;}return 0;}

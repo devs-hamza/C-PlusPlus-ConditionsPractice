403d52e7d1d7900ea9f9d20e052d67897f87618a
Hamza <alihamza031340@gmail.com>
2026-09-24 21:26:09 +0500

    Organize Practice-42.cpp

---
+++ If-Else-If-Else/Practice-42.cpp
@@ -0,0 +1,4 @@
+// Count Number of Digits
+#include <iostream>
+using namespace std;
+int main(){int num; cout<<"Enter a number: "; cin>>num; if(num>=0&&num<=9)cout<<"Number of digits = 1"; else if(num>=10&&num<=99)cout<<"Number of digits = 2"; else if(num>=100&&num<=999)cout<<"Number of digits = 3"; else if(num>=1000&&num<=9999)cout<<"Number of digits = 4"; else cout<<"Number has 5 or more digits"; return 0;}

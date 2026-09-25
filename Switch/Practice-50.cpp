171ab2d8a592b57bc6468bd327e9feb054719b65
Hamza <alihamza031340@gmail.com>
2026-09-25 08:22:20 +0500

    Organize Practice-50.cpp into Switch

---
+++ Switch/Practice-50.cpp
@@ -0,0 +1,4 @@
+// Traffic Light System
+#include <iostream>
+using namespace std;
+int main(){char light;cout<<"Enter traffic light (R/Y/G): ";cin>>light;switch(light){case 'R':case 'r':cout<<"Stop";break;case 'Y':case 'y':cout<<"Ready";break;case 'G':case 'g':cout<<"Go";break;default:cout<<"Invalid traffic light";}return 0;}

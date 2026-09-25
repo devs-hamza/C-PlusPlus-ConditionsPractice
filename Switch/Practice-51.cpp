3fe3fd700b87db457eff8d61b7e755da1a4ceead
Hamza <alihamza031340@gmail.com>
2026-09-25 08:47:45 +0500

    Format Practice-51.cpp for readability

--- Switch/Practice-51.cpp
+++ Switch/Practice-51.cpp
@@ -1,4 +1,39 @@
 // Display Grade Description
 #include <iostream>
 using namespace std;
-int main(){char grade;cout<<"Enter your grade (A/B/C/D/F): ";cin>>grade;switch(grade){case 'A':case 'a':cout<<"Excellent";break;case 'B':case 'b':cout<<"Very Good";break;case 'C':case 'c':cout<<"Good";break;case 'D':case 'd':cout<<"Needs Improvement";break;case 'F':case 'f':cout<<"Fail";break;default:cout<<"Invalid Grade";}return 0;}
+
+int main()
+{
+    char grade;
+
+    cout << "Enter your grade (A/B/C/D/F): ";
+    cin >> grade;
+
+    switch(grade)
+    {
+    case 'A':
+    case 'a':
+        cout << "Excellent";
+        break;
+    case 'B':
+    case 'b':
+        cout << "Very Good";
+        break;
+    case 'C':
+    case 'c':
+        cout << "Good";
+        break;
+    case 'D':
+    case 'd':
+        cout << "Needs Improvement";
+        break;
+    case 'F':
+    case 'f':
+        cout << "Fail";
+        break;
+    default:
+        cout << "Invalid Grade";
+    }
+
+    return 0;
+}

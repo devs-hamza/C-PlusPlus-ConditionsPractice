19d2714ceea18ba55a6f77c1e2b9eadb5dbeb16a
Hamza <alihamza031340@gmail.com>
2026-09-25 08:22:29 +0500

    Organize Practice-54.cpp into Switch

---
+++ Switch/Practice-54.cpp
@@ -0,0 +1,4 @@
+// Check Vowel Using Switch
+#include <iostream>
+using namespace std;
+int main(){char ch;cout<<"Enter a character: ";cin>>ch;switch(ch){case 'a':case 'e':case 'i':case 'o':case 'u':case 'A':case 'E':case 'I':case 'O':case 'U':cout<<"It is a vowel";break;default:cout<<"It is not a vowel";}return 0;}

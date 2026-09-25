5d44665798db64aff1b8e40db204812a2fa3a905
Hamza <alihamza031340@gmail.com>
2026-09-25 08:48:10 +0500

    Format Practice-59.cpp for readability

--- Switch/Practice-59.cpp
+++ Switch/Practice-59.cpp
@@ -1,4 +1,35 @@
 // Student Information Menu
 #include <iostream>
 using namespace std;
-int main(){int choice;string name="Ali";int rollNo=25,marks=85;cout<<"===== Student Information =====\n1. Display Name\n2. Display Roll Number\n3. Display Marks";cout<<"\nEnter your choice: ";cin>>choice;switch(choice){case 1:cout<<"Name = "<<name;break;case 2:cout<<"Roll Number = "<<rollNo;break;case 3:cout<<"Marks = "<<marks;break;default:cout<<"Invalid choice";}return 0;}
+
+int main()
+{
+    int choice;
+    string name = "Ali";
+    int rollNo = 25, marks = 85;
+
+    cout << "===== Student Information =====\n";
+    cout << "1. Display Name\n";
+    cout << "2. Display Roll Number\n";
+    cout << "3. Display Marks";
+
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+
+    switch(choice)
+    {
+    case 1:
+        cout << "Name = " << name;
+        break;
+    case 2:
+        cout << "Roll Number = " << rollNo;
+        break;
+    case 3:
+        cout << "Marks = " << marks;
+        break;
+    default:
+        cout << "Invalid choice";
+    }
+
+    return 0;
+}

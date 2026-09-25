08984ff925ef6a3365b8924e7ede7c7a4bb7e948
Hamza <alihamza031340@gmail.com>
2026-09-25 08:22:14 +0500

    Organize Practice-47.cpp into Switch

---
+++ Switch/Practice-47.cpp
@@ -0,0 +1,4 @@
+//Display Month According to Month Number
+#include <iostream>
+using namespace std;
+int main(){cout<<"Enter the number of the Month : ";int month;cin>>month;switch(month){case 1:cout<<"It's January";break;case 2:cout<<"It's February";break;case 3:cout<<"It's March";break;case 4:cout<<"It's April";break;case 5:cout<<"It's May";break;case 6:cout<<"It's June";break;case 7:cout<<"It's July ";break;case 8:cout<<"It's August";break;case 9:cout<<"It's Septenber";break;case 10:cout<<"It's October";break;case 11:cout<<"It's November";break;case 12:cout<<"It's December";break;default:cout<<"Error";}return 0;}

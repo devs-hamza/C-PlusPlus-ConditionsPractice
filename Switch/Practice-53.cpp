abb1f2a9c980c5f731739b8bfde92d54581cf41d
Hamza <alihamza031340@gmail.com>
2026-09-25 08:22:26 +0500

    Organize Practice-53.cpp into Switch

---
+++ Switch/Practice-53.cpp
@@ -0,0 +1,4 @@
+// Menu-Driven Calculator
+#include <iostream>
+using namespace std;
+int main(){int choice;double num1,num2;cout<<"Enter first number: ";cin>>num1;cout<<"Enter second number: ";cin>>num2;cout<<"\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division";cout<<"\nEnter your choice: ";cin>>choice;switch(choice){case 1:cout<<"Result = "<<num1+num2;break;case 2:cout<<"Result = "<<num1-num2;break;case 3:cout<<"Result = "<<num1*num2;break;case 4:if(num2!=0)cout<<"Result = "<<num1/num2;else cout<<"Cannot divide by zero";break;default:cout<<"Invalid choice";}return 0;}
